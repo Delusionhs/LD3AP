1159d85b32485964c4ac4a1b22d4461bd770d4f2
Igor Podolskiy <delusionhs@mail.ru>
2019-01-29 12:24:34 +1100

    new script blocks

--- LD3ADM-UI.rb
+++ LD3ADM-UI.rb
@@ -75,6 +75,11 @@ class MainWindow < FXMainWindow
                  :width => 250, :height => 40)
     unblockButton.connect(SEL_COMMAND) {  buttonDML(3, textField.getText) }
 
+    cardInkButton = FXButton.new(controls, "Очистить и разболокировать КВК", :opts => FRAME_RAISED|FRAME_THICK|LAYOUT_LEFT|
+        LAYOUT_CENTER_Y|LAYOUT_FIX_WIDTH|LAYOUT_FIX_HEIGHT,
+                                 :width => 250, :height => 40)
+    cardInkButton.connect(SEL_COMMAND) {  buttonDML(6, textField.getText) }
+
     #fileButton = FXButton.new(controls, "Удаление файла", :opts => FRAME_RAISED|FRAME_THICK|LAYOUT_LEFT|
     #    LAYOUT_CENTER_Y|LAYOUT_FIX_WIDTH|LAYOUT_FIX_HEIGHT,
     #             :width => 250, :height => 40)
